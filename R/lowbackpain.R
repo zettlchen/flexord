a548064e2a4e0c31e9581c406b2f1b0355a7e5f9
bettina.gruen <bettina.gruen@wu.ac.at>
2025-03-05 16:24:57 +0100

    man page improved for lowbackpain

--- R/lowbackpain.R
+++ R/lowbackpain.R
@@ -1,6 +1,6 @@
 #' Low Back Pain Diagnoses and Diagnosis Criteria
 #' 
-#' In a 2011 study, Smart et al. collected information on 464 Irish and
+#' In a 2011 study, Smart et al. (2011) collected information on 464 Irish and
 #' British patients suffering from low back pain regarding:
 #' - the type of low back pain (classified into 'nociceptive', 'peripheral
 #'   neuropathic', and 'central neuropathic')
@@ -15,7 +15,7 @@
 #' @usage data('lowbackpain')
 #' 
 #' @format
-#' The dataset `lowbackpain` provided with this package is a list containing:
+#' A list containing:
 #' \describe{
 #'  \item{data:}{A 464x11 binary matrix indicating the presence/absence of the
 #'              11 selected criteria for each of the 464 patients. Rownames are
@@ -30,11 +30,13 @@
 #' - Fop, M, Smart, K, Murphy, TB (2017).
 #'   *Variable Selection for Latent Class Analysis with Application to Low Back Pain Diagnosis*.
 #'   The Annals of Applied Statitics. 11(4), 2080-2110.
-#'   \doi{doi:10.1214/17-aoas1061}
+#'   \doi{10.1214/17-aoas1061}
 #' - Smart, K, Blake, C, Staines, A, Doody, C (2011).
 #'   *The Discriminative Validity of "Nociceptive", "Peripheral Neuropathic", and "Central Sensitization" as Mechanisms-Based Classifications of Musculoskeletal Pain*.
 #'   The Clinical Journal of Pain. 27, 655-663.
-#'   \doi{doi:10.1097/AJP.0b013e318215f16a}
+#'   \doi{10.1097/AJP.0b013e318215f16a}
 #'   
-#' @source <https://projecteuclid.org/journals/annals-of-applied-statistics/volume-11/issue-4/Variable-selection-for-latent-class-analysis-with-application-to-low/10.1214/17-AOAS1061.full?tab=ArticleLinkSupplemental>
+#' @source
+#'
+#' Supplemental Content for Fop et al. (2017): \doi{10.1214/17-AOAS1061SUPP}
 "lowbackpain"
