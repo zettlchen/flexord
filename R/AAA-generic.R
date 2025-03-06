74f75c25af28db8d649fb363ebad67c78db8d06f
bettina.gruen <bettina.gruen@wu.ac.at>
2025-03-06 16:49:03 +0100

    removed .ScaleGower and improved .rangeMatrix

--- R/AAA-generic.R
+++ R/AAA-generic.R
@@ -15,54 +15,29 @@
 #        list(x1= c(lower, upper), x2=c(lower, upper), ...): list with user
 #                 specified range vectors for each variable to be scaled.
 .rangeMatrix <- function(xrange) {
-  if(all(xrange=='all')) {
+  if (identical(xrange, 'all')) {
     rng <- function(x) {
-      rep(range(x, na.rm=T), ncol(x)) |>
-        matrix(nrow=2)
+      rep(range(x, na.rm=TRUE), ncol(x)) |>
+          matrix(nrow=2)
     }
-  } else if(all(xrange=='columnwise')) {
+  } else if (identical(xrange, 'columnwise')) {
     rng <- function(x) {
-      apply(x, 2, range, na.rm=T)
+        apply(x, 2, range, na.rm=TRUE)
     }
-  } else if(is.vector(xrange, mode='numeric')) {
+  } else if (is.vector(xrange, mode='numeric')) {
     rng <- function(x) {
-      rep(xrange, ncol(x)) |>
-        matrix(nrow=2)
+        rep(xrange, ncol(x)) |>
+            matrix(nrow=2)
     }
+  } else if (is.list(xrange)) {
+    rng <- function(x) {
+        if (length(xrange) != ncol(x))
+            stop('Either supply 1 range vector, or list of ranges for all variables')
+        unlist(xrange) |> matrix(nrow=2)
+    } 
   } else {
-    rng <- function(x) {
-      if(length(xrange) != ncol(x))
-        stop('Either supply 1 range vector, or list of ranges for all variables')
-      unlist(xrange) |> matrix(nrow=2)
-    }
+      stop("xrange not correctly specified")
   }
   return(rng)
 }
 
-#archived scaling option after Gower that scales over all variable types.
-#Easier to code (cause I don't need to extract xclass from the parent frame),
-#and works in cases where xrange is variable specific ('columnwise', or list of range vectors).
-#However, in the case of data wide ranges, it destroys the binary variables.
-# @param x a numerically coded matrix.
-# @param rangeMatrix expects a function that has been previously created
-#                    with `.rangeMatrix(xrange)`. If it is NULL, it is created on x.
-# @param xrange is a compatibility parameter so the helper runs outside
-#               of the `kccaFamilyGower` concept, but within `kccaFamilyGower`,
-#               `.rangeMatrix(xrange)` is run previously
-.ScaleGower <- function(x, rangeMatrix=NULL,
-                        xrange=NULL) {
-  
-  if(is.null(rangeMatrix)) {
-    rng <- .rangeMatrix(xrange)
-  } else {
-    rng <- rangeMatrix
-  }
-  
-  if(is.null(colnames(x))) colnames(x) <- 1:ncol(x) #don't need this here but later in the dist
-  
-  rng <- rng(x)
-  scl <- apply(rng, 2, diff)
-  scl <- ifelse(scl==0, 1, scl)
-  
-  scale(x, center=rng[1,], scale=scl)
-}
