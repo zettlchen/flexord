3422d03bf879d1caf094dfe2635fb8e9556f341e
Dominik Ernst <de@dernst.org>
2025-03-11 19:29:29 +0100

    apparently renaming files fudges up your git history

--- R/flxregbinom.R
+++ R/flxregbinom.R
@@ -1,58 +1,61 @@
 #' FlexMix Driver for Regularized Binomial Mixtures
 #'
-#' This model driver can be used to cluster binomial data.
-#' Using a regularization parameter `alpha` greater than zero
-#' acts as adding `alpha` observations conforming to the population
-#' mean to each component. This can be used to avoid degenerate
-#' solutions (i.e. probabilites of 0 or 1). It also has the effect
-#' that clusters become more similar to each other the larger
-#' `alpha` is chosen. For small values it is mostly negligible however.
+#' This model driver can be used to cluster data using the binomial
+#' distribution.
 #'
-#' Parameter estimation is achieved using the MAP estimator for each cluster
-#' and variable using a Beta prior.
-#' @param size number of trials (zero or more)
-#' @param alpha Regularization parameter. Can be regarded the same as
-#'  adding `alpha` observations conforming to the population mean to each
-#'  component.
-#' @param eps When greater than zero, probabilities are truncated to fit
-#'     in \[eps, 1-eps\]
-#' @param formula A formula which is interpreted relative to the formula
-#'        specified in the call to [flexmix::flexmix()] using
-#'        [stats::update.formula()]. Only the left-hand side (response)
-#'        of the formula is used. Default is to
-#'        use the original [flexmix::flexmix()] model formula.
+#' Using a regularization parameter `alpha` greater than zero can be
+#' viewed as adding `alpha` observations equal to the population mean
+#' to each component. This can be used to avoid degenerate solutions
+#' (i.e., probabilites of 0 or 1). It also has the effect that
+#' clusters become more similar to each other the larger `alpha` is
+#' chosen. For small values this effect is, however, mostly
+#' negligible.
+#'
+#' Parameter estimation is achieved using the MAP estimator for each
+#' component and variable using a Beta prior.
+#' 
+#' @param size Number of trials (one or more).
+#' @param alpha A non-negative scalar acting as regularization
+#'     parameter. Can be regarded as adding
+#'     `alpha` observations equal to the population mean to each
+#'     component.
+#' @param eps A numeric value in [0, 1). When greater than zero,
+#'     probabilities are truncated to be within in \[eps, 1-eps\].
+#' @param formula A formula which is interpreted relative to the
+#'     formula specified in the call to [flexmix::flexmix()] using
+#'     [stats::update.formula()]. Only the left-hand side (response)
+#'     of the formula is used. Default is to use the original model
+#'     formula specified in [flexmix::flexmix()].
 #' @return an object of class `"FLXC"`
 #' @export
-#' @references
-#' - Ernst, D, Ortega Menjivar, L, Scharl T, Grün, B (2025).
-#'   *Ordinal clustering with the flex-Scheme.*
-#'   Austrian Statistics Journal. _Submitted manuscript_.
+#' @references - Ernst, D, Ortega Menjivar, L, Scharl, T, Grün, B
+#'     (2025).  *Ordinal Clustering with the flex-Scheme.* Austrian
+#'     Journal of Statistics. _Submitted manuscript_.
 #' @example examples/binomial.R
-FLXMCregbinom = function(formula=.~., size=NULL, alpha=0, eps=0)
+#' @importFrom stats dbinom
+FLXMCregbinom = function(formula=.~., size = NULL, alpha=0, eps=0)
 {
     z <- new("FLXMC", weighted=TRUE, formula=formula,
-             name="FLXMCbinomial")
+             name="FLXMCregbinom")
 
+    stopifnot(is.numeric(eps), length(eps) == 1, eps >= 0, eps < 1)
+    stopifnot(is.numeric(alpha), length(alpha) == 1, alpha >= 0)
+    
     z@preproc.y <- function(y) {
         if(any(y < 0, na.rm=TRUE))
             stop("negative values are not allowed for the binomial family")
         y
     }
 
-    defineComponent <- function(component, probs, df) {
+    defineComponent <- function(component) {
         predict <- function(x, ...) {
-            stop("not implemented")
+            matrix(component$probs * component$size, nrow = nrow(x), ncol = length(component$probs),
+                   byrow = TRUE)
         }
 
         logLik <- function(x, y) {
-            probs = component$probs
-            ty <- t(y)
-            bc <- log(choose(size, ty))
-
-            l <- bc + ty*log(probs) + (size-ty)*log(1-probs)
-            cs <- colSums(l, na.rm=TRUE)
-            if(any(!is.finite(cs))) stop("non-finite values while calculating log-likelihood")
-            cs
+            llh <- dbinom(t(y), size = component$size, prob = component$probs, log = TRUE)
+            colSums(llh, na.rm = TRUE)
         }
 
         new("FLXcomponent",
@@ -63,8 +66,7 @@ FLXMCregbinom = function(formula=.~., size=NULL, alpha=0, eps=0)
 
     z@fit <- function(x, y, w, component) {
         if(length(component) == 0) {
-            component$has_na <- any(is.na(y))
-            component$which_na <- which(is.na(y))
+            component$has_na <- anyNA(y)
 
             if(is.null(size)) {
                 component$size <- apply(y, 2, max, na.rm=TRUE)
@@ -77,15 +79,13 @@ FLXMCregbinom = function(formula=.~., size=NULL, alpha=0, eps=0)
         }
 
         if(component$has_na) {
-            #w <- matrix(w, nrow=length(w), ncol=ncol(y))
-            #w[which_na] <- NA
             p <- with(component,
                       (b_alpha + colSums(w*y, na.rm=TRUE)) /
-                      (b_alpha+b_beta+size*colSums(w, na.rm=TRUE)))
+                      (b_alpha+b_beta+size*colSums(w * !is.na(y))))
         } else {
             p <- with(component,
-                      (b_alpha + colSums(w*y, na.rm=TRUE)) /
-                      (b_alpha+b_beta+size*sum(w, na.rm=TRUE)))
+                      (b_alpha + colSums(w*y)) /
+                      (b_alpha+b_beta+size*sum(w)))
         }
 
         if(eps > 0) {
