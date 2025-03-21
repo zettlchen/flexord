2c91179e7e31a1b88b39dea11874e219fb270b1b
bettina.gruen <bettina.gruen@wu.ac.at>
2025-03-21 16:05:20 +0100

    slight changes to regnorm

--- R/flxregnorm.R
+++ R/flxregnorm.R
@@ -2,12 +2,12 @@
 #'
 #' This model driver implements the regularization method as
 #' introduced by Fraley and Raftery (2007) for univariate normal
-#' mixtures. Default parameters for the regularization are taken from
-#' that paper.  We extend this to the multivariate case assuming
-#' independence between variables within components, i.e., we only
-#' implement the special case where the covariance matrix is
-#' diagonal. For more general applications of normal mixtures see
-#' package \pkg{mclust}.
+#' mixtures. Default parameters for the regularization according to
+#' that paper may be obtained using `FLXMCregnorm_defaults()`. We
+#' extend this to the multivariate case assuming independence between
+#' variables within components, i.e., we only implement the special
+#' case where the covariance matrix is diagonal. For more general
+#' applications of normal mixtures see package \pkg{mclust}.
 #'
 #' For the regularization the conjugate prior distributions for the
 #' normal distribution are used, which are:
@@ -16,22 +16,14 @@
 #' * Inverse Gamma prior with parameters `nu_p/2` and `zeta_p^2/2` for the
 #'   variance.
 #'
-#' `mu_p` is computed from the data as the overall means across all components.
-#'
-#' A value for the scale hyperparameter `zeta_p` may be specified directly.
-#' Otherwise the empirical variance divided by the square of the number of
-#' components is used as per Fraley and Raftery (2007). In which case the
-#' number of components (parameter `G`) needs to be specified.
-#' 
-#'
 #' @param formula A formula which is interpreted relative to the formula
 #'        specified in the call to [flexmix::flexmix()] using
 #'        [stats::update.formula()]. Only the left-hand side (response)
 #'        of the formula is used. Default is to
 #'        use the original model formula specified in [flexmix::flexmix()].
-#' @param params Prior parameters for normal mixtures. You may obtain default
-#'        defaults from Fraley and Raftery (2007) using the function
-#'        `FLXMCregnorm_defaults`.
+#' @param params Prior parameters for normal mixtures. You may obtain 
+#'        default values according to Fraley and Raftery (2007) using 
+#'        `FLXMCregnorm_defaults()`.
 #'        As the prior depends on the number of components it is probably not
 #'        advisable to run `stepFlexmix` with more than one value of `k` at
 #'        a time.
@@ -63,31 +55,22 @@ FLXMCregnorm <- function(formula=.~., params) {
         }
 
         new("FLXcomponent",
-            parameters=list(center=para$center, s2=para$s2,
-                            mu_p = para$mu_p, var_data = para$var_data),
+            parameters=list(center = para$center, s2 = para$s2),
             logLik=logLik, df=para$df, 
             predict=predict)
     }
 
     z@fit <- function(x, y, w, component=NULL) {
-        # From Fraley/Raftery
-        n = nrow(y)
+        nk <- sum(w)
+        ykbar <- colSums(w*y)/nk
 
-        if(length(component) == 0L) {
-            #component = params
-        }
+        muhat1 <- (nk*ykbar + params$kappa_p*params$mu_p)/(params$kappa_p + nk)
 
-        nk = sum(w)
-        ykbar = colSums(w*y)/nk
-
-        muhat1 = (nk*ykbar + params$kappa_p*params$mu_p)/(params$kappa_p + nk)
-
-        s2hat_numer1 =
-            params$zeta_p +
+        s2hat_numer1 <- params$zeta_p +
             (params$kappa_p*nk)/(params$kappa_p+nk)*(ykbar - params$mu_p)^2
-        s2hat_numer2 = rowSums(w * (t(y) - ykbar)^2)
-        s2hat_denom = params$nu_p + nk + 3
-        s2hat = (s2hat_numer1+s2hat_numer2) / s2hat_denom
+        s2hat_numer2 <- rowSums(w * (t(y) - ykbar)^2)
+        s2hat_denom <- params$nu_p + nk + 3
+        s2hat <- (s2hat_numer1 + s2hat_numer2) / s2hat_denom
 
 
         para = list(center = muhat1, s2 = s2hat, df = 2*ncol(y))
@@ -98,42 +81,57 @@ FLXMCregnorm <- function(formula=.~., params) {
 }
 
 
-#' Default parameters for regularized normal mixtures based on the data set
-#' @param x The data set to be clustered. Should be the same data set as is 
-#'          used in flexmix's model formula.
-#' @param G Number of components in the mixture model (not used if zeta_p is given)
-#' @param kappa_p Shrinkage parameter. Functions as if you added
-#'                `kappa_p` observations according to the population mean to
-#'                each component (hyperparameter for IG prior)
+#' Data-Driven Default Parameters for Regularized Normal Mixtures 
+#'
+#' Determines the default values for regularized univariate normal
+#' mixtures as proposed by Fraley and Raftery (2007) based on the data
+#' set to be clustered and the number of components in the mixture
+#' model.
+#' 
+#' @param x The data set to be clustered. Should be the same data set
+#'     as is used in [flexmix::flexmix()]'s model formula.
+#' @param k Number of components assumed for the mixture model (not
+#'     used if `zeta_p` is given).
+#' @param kappa_p Shrinkage parameter. Corresponds to adding `kappa_p`
+#'     observations according to the population mean to each component
+#'     (hyperparameter for IG prior)
 #' @param nu_p Degress of freedom (hyperparameter for IG prior)
-#' @param zeta_p Scale (hyperparameter for IG prior). If not given the empirical
-#'             variance divided by the square of the number of components
-#'             is used as per Fraley and Raftery (2007).
+#' @param zeta_p Scale (hyperparameter for IG prior). If not given the
+#'     empirical variance divided by the square of the number of
+#'     components is used as per Fraley and Raftery (2007).
+#'
+#' `mu_p` is computed from the data as the overall means across all
+#'     components.
+#'
+#' A value for the scale hyperparameter `zeta_p` may be specified directly.
+#' Otherwise the empirical variance divided by the square of the number of
+#' components is used as per Fraley and Raftery (2007). In which case the
+#' number of components (parameter `k`) needs to be specified.
+#' 
 #' @export
-FLXMCregnorm_defaults <- function(x, zeta_p=NULL, kappa_p=0.01, nu_p=3, G=NULL) {
-    if(is.null(zeta_p) && is.null(G)) {
-        stop("either parameter zeta_p or G is needed")
+FLXMCregnorm_defaults <- function(x, zeta_p=NULL, kappa_p=0.01, nu_p=3, k=NULL) {
+    if (is.null(zeta_p) && is.null(k)) {
+        stop("either parameter zeta_p or k is needed")
     }
+    stopifnot(is.numeric(kappa_p), length(kappa_p) == 1)
+    stopifnot(is.numeric(nu_p), length(nu_p) == 1)
 
-    params = list()
-    params$mu_p = colMeans(x)
+    params <- list(mu_p = colMeans(x),
+                   kappa_p = kappa_p,
+                   nu_p = nu_p)
 
-    n = nrow(x)
+    if (is.null(zeta_p)) {
+        stopifnot(is.numeric(k), length(k) == 1)
+        stopifnot(is.matrix(x) || is.data.frame(x))
+        k <- as.integer(k)
+        n <- nrow(x)
+        var_data <- 1/(n-1) * vapply(seq_len(ncol(x)), \(i) {
+            sum((x[,i] - params$mu_p[i])^2)
+        }, double(1))
+        zeta_p <- var_data / k^2
+    } 
 
-    params$var_data = 1/(n-1) * vapply(seq_len(ncol(x)), \(i) {
-        sum((x[,i] - params$mu_p[i])^2)
-    }, double(1))
-
-    if(is.null(zeta_p) && !is.null(G)) {
-        zeta_p2 = params$var_data / G^2
-    } else {
-        zeta_p2 = zeta_p
-    }
-
-
-    params$kappa_p = kappa_p
-    params$nu_p = nu_p
-    params$zeta_p = zeta_p2
+    params$zeta_p <- zeta_p
     params
 }
 
--- examples/regnorm.R
+++ examples/regnorm.R
@@ -8,32 +8,22 @@ my_iris <- subset(iris, select=setdiff(colnames(iris), "Species")) |>
     as.matrix()
 
 # cluster one model with a scale parameter similar to the default for 3 components
-m1 <- stepFlexmix(
-    my_iris~1,
-    model=FLXMCregnorm(
-        params=FLXMCregnorm_defaults(my_iris, zeta_p=c(0.23, 0.06, 1.04, 0.19))
-    ),
-    k=3)
-
-
+params <- FLXMCregnorm_defaults(my_iris, zeta_p = c(0.23, 0.06, 1.04, 0.19))
+m1 <- stepFlexmix(my_iris ~ 1, k = 3, 
+    model=FLXMCregnorm(params = params))
 summary(m1)
 
 # rand index of clusters vs species
 randIndex(clusters(m1), iris$Species)
 
 # cluster one model with default scale parameter
-m2 <- stepFlexmix(
-    my_iris~1,
-    model=FLXMCregnorm(
-        params=FLXMCregnorm_defaults(my_iris, G=3)
-    ),
-    k=3)
-
+params <- FLXMCregnorm_defaults(my_iris, k = 3)
+m2 <- stepFlexmix(my_iris ~ 1, k = 3,
+    model = FLXMCregnorm(params = params))
 summary(m2)
 
 # rand index of clusters vs species
 randIndex(clusters(m2), iris$Species)
-
 
 # rand index between both models (should be >= 0.8)
 randIndex(clusters(m1), clusters(m2))
